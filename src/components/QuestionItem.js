8b786c9e7efe4959e3396e61ef1072bd4f6367c9
Andrew Osborn <osbornandrewj@gmail.com>
2022-07-21 13:18:17 -0700

    commit tree 09f26c4a6f8adc8e92538bd753e8df65862312c8

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
-
-function QuestionItem({ question }) {
+//set function with components to pass as props to QuestionList 
+function QuestionItem({ question, onDelete, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,16 +8,28 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
-
+  //create delete callBack function
+  function  handleDelete(){
+    onDelete(id);
+  }
+  //do same for answering question
+  function handleAnswerChange(e) {
+    onAnswerChange(id, parseInt(e.target.value));
+  }
+  //JSX format to represent individual questions
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select 
+          defaultValue={correctIndex}
+          onChange={handleAnswerChange}>
+            {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick = {handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
+
 
 function QuestionList() {
+  //Establish state to represent questions
+  const [questions, setQuestions] = useState([]);
+  //GET questions from server
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => {
+        setQuestions(questions);
+      });
+  }, []);
+
+  //Establish Method to DELETE questions from the server
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+    })
+        .then((r) => r.json())
+        .then(() => {
+          //const for updatedQuestions filter out selected data and redo state with removed data
+          const updatedQuestions = questions.filter((q) => q.id !== id);
+          setQuestions(updatedQuestions);
+        });
+  }
+
+  //Use a PATCH fetch request to send answer and update appropriatly
+  function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+        .then((r) => r.json())
+        .then((updatedQuestion) => {
+          const updatedQuestions = questions.map((q) => {
+            if(q.id === updatedQuestion.id) return updatedQuestion;
+            return q
+          });
+          setQuestions(updatedQuestions)
+        });
+  }
+  //set up JSX to represent questions and added functionalities 
+  const questionItems = questions.map((q) => (
+    <QuestionItem
+      key = {q.id}
+      question = {q}
+      onDelete={handleDelete}
+      onAnswerChange={handleAnswerChange}
+    />
+  ))
+
+  //establish final JSX to pass to App component for rendering in DOM
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
